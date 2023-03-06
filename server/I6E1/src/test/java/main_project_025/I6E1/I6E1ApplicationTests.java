a7ae0dae16eda372f7626e133ceda392bb324e81
Eunjinshin <58281884+Eunjinshin@users.noreply.github.com>
2023-03-06 16:56:08 +0900

    서버 기본 셋팅

---
+++ server/I6E1/src/test/java/main_project_025/I6E1/I6E1ApplicationTests.java
@@ -0,0 +1,13 @@
+package main_project_025.I6E1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I6E1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
