6f4abbcb4353e1111a6d9478c7d56f8f38d6f176
Diiiiiikey <chest6654@gmail.com>
2023-03-08 14:05:20 +0900

    :lipstick: theme 추가

---
+++ client/src/style/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  windowSize: {
+    small: "screen and (max-width: '680px')",
+    base: "screen and (max-width: '980px')",
+    large: "screen and (max-width: '1280px')",
+  },
+  themeColor: { milkTea: '#ddba9d' },
+};
+
+export default theme;
