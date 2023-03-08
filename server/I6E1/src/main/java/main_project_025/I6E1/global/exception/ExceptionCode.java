fd11677b9087336c6821feb0b3cbcd31e5a96afb
Dev_Hojju <115970786+hojju@users.noreply.github.com>
2023-03-08 13:43:56 +0900

    Update ExceptionCode.java

--- server/I6E1/src/main/java/main_project_025/I6E1/global/exception/ExceptionCode.java
+++ server/I6E1/src/main/java/main_project_025/I6E1/global/exception/ExceptionCode.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 public enum ExceptionCode {
 
     COMMISSION_NOT_FOUND(404, "게시글을 찾지 못하였습니다."),
-    COMMISSION_EXIST(200,"댓글이 존재합니다"),
     NOT_AUTHORITY(403, "권한이 없습니다."),
     USER_NOT_FOUND(404, "User not found");
 
