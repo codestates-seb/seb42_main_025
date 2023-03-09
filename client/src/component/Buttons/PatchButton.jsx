7d35ffede53f5e6ed3b22bd48d04dd4108ed28d9
Diiiiiikey <chest6654@gmail.com>
2023-03-09 11:52:41 +0900

    :recycle: Buttons 수정 및 Patch 버튼 추가

--- client/src/component/Buttons/PatchButton.jsx
+++ client/src/component/Buttons/PatchButton.jsx
@@ -2,17 +2,18 @@ import styled, { css } from 'styled-components';
 
 const S_Container = styled.div`
   ${({ theme, color }) => {
-    const selectColor = color || theme.themeColor.milkTea;
+    const selectColor = color || theme.themeColor.darkGray;
+    const selectBorder = theme.themeColor.lightGray;
     return css`
       > button {
         padding: 0.7rem 1rem;
         margin: 0.5rem;
-        border: none;
-        border-radius: 5px;
+        border: 1px solid ${selectBorder};
+        border-radius: 30px;
         white-space: nowrap;
         font-weight: bold;
-        color: #fff;
-        background-color: ${selectColor};
+        color: ${selectColor};
+        background-color: #fff;
         cursor: pointer;
         :hover {
           filter: brightness(0.9);
@@ -26,7 +27,7 @@ const S_Container = styled.div`
   }}
 `;
 
-function PostButton({ value, onClick, propsColor }) {
+function PatchButton({ value, onClick, propsColor }) {
   return (
     <S_Container color={propsColor}>
       <button onClick={onClick}>{value}</button>
@@ -34,4 +35,4 @@ function PostButton({ value, onClick, propsColor }) {
   );
 }
 
-export default PostButton;
+export default PatchButton;
