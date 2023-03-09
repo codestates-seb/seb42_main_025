dace61588c4ecc0c7cdb653f4f1cfff4cfc80ac0
Diiiiiikey <chest6654@gmail.com>
2023-03-09 12:02:07 +0900

    :lipstick: theme 색상 추가

--- client/src/style/theme.js
+++ client/src/style/theme.js
@@ -4,7 +4,13 @@ const theme = {
     base: "screen and (max-width: '980px')",
     large: "screen and (max-width: '1280px')",
   },
-  themeColor: { milkTea: '#ddba9d', milkTeaTwo: '#c29567', headerGray: '#c1c1c1' },
+  themeColor: {
+    milkTea: '#ddba9d',
+    milkTeaTwo: '#c29567',
+    lighterGray: '#fcfafa',
+    lightGray: '#c1c1c1',
+    darkGray: '#313131',
+  },
 };
 
 export default theme;
