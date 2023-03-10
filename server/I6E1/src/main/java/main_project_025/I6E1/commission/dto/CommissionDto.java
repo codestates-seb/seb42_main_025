b4d1e8b279a0a70c77a4f46c775d4f062a2660f6
hojju <hojju0902@gmail.com>
2023-03-10 16:14:03 +0900

    ✨feat : Commission CRUD_v1

---
+++ server/I6E1/src/main/java/main_project_025/I6E1/commission/dto/CommissionDto.java
@@ -0,0 +1,46 @@
+package main_project_025.I6E1.commission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+public class CommissionDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Post{
+        @NotBlank(message = "제목을 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Patch{
+        @NotBlank(message = "제목을 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Response{
+        private long commissionId;
+        private String title;
+        private String content;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+        private String memberName;
+        private String memberEmail;
+
+        //private List<ReviewResponseDto> reviews;
+    }
+}
--- server/I6E1/src/main/java/main_project_025/I6E1/commission/entity/Commission.java
+++ server/I6E1/src/main/java/main_project_025/I6E1/commission/entity/Commission.java
@@ -4,17 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 import lombok.NoArgsConstructor;
+import main_project_025.I6E1.Member.entity.Member;
 import main_project_025.I6E1.global.auditable.Auditable;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table
 @Setter @Getter
 @Where(clause = "deleted=false")
-@SQLDelete(sql = "UPDATE commission SET deleted = true WHERE id=?")
+@SQLDelete(sql = "UPDATE commission SET deleted = true WHERE commission_id=?")
 @NoArgsConstructor
 public class Commission extends Auditable {
     @Id
@@ -27,11 +26,10 @@ public class Commission extends Auditable {
     @Column(columnDefinition = "mediumtext", nullable = false)
     private String content;
 
-    /* 엔티티 미구현
-    @ManyToOne(targetEntity = User.class, cascade = CascadeType.PERSIST)
+    @ManyToOne(targetEntity = Member.class, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "USER_ID")
-    private User user;
-    */
+    private Member member;
+
 
     /*  엔티티 미구현
     @OneToMany(mappedBy = "review", cascade = CascadeType.PERSIST)
