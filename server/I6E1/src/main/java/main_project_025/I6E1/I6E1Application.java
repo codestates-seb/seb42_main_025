41abd779436fdef6ed8b38bd359c873ddf65e8bf
hojju <hojju0902@gmail.com>
2023-03-10 16:14:36 +0900

    ✨feat : Main  @EnableJpaAuditing 추가

--- server/I6E1/src/main/java/main_project_025/I6E1/I6E1Application.java
+++ server/I6E1/src/main/java/main_project_025/I6E1/I6E1Application.java
@@ -1,11 +1,13 @@
 package main_project_025.I6E1;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class I6E1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(I6E1Application.class, args);
 	}
