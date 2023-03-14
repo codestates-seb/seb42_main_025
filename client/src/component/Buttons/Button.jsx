31ae02bb6096449f419e2a5d5aa04a785db9e89f
benidene <dlgusehd09@naver.com>
2023-03-14 15:23:13 +0900

    ✨ : height 추가

--- client/src/component/Buttons/Button.jsx
+++ client/src/component/Buttons/Button.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 
 const StyledButton = styled.button`
   width: ${props => props.width || 'inherit'};
-  padding: ${props => props.padding || '8px 10px'};
+  height: ${props => props.height || 'inherit'};
+  padding: ${props => props.padding || '4px 8px'};
   border: ${props => (props.bdColor ? 'solid 1px' : 'none')};
   border-radius: ${props => props.borderRadius || '10px'};
   font-size: ${props => props.fontSize || '16px'};
@@ -18,7 +19,7 @@ const StyledButton = styled.button`
 
 function Button({ text, path = '', handleClick, addStyle = {} }) {
   const navigate = useNavigate();
-  const { padding, width, borderRadius, fontSize } = addStyle;
+  const { padding, width, borderRadius, fontSize, height } = addStyle;
 
   const goTo = path => {
     navigate(path);
@@ -27,6 +28,7 @@ function Button({ text, path = '', handleClick, addStyle = {} }) {
   return (
     <StyledButton
       width={width}
+      height={height}
       padding={padding}
       borderRadius={borderRadius}
       fontSize={fontSize}
@@ -43,8 +45,8 @@ export default Button;
 
 {
   /* <Button
-text='Ask Question' // 텍스트 값
-path='/questions/ask' // 클릭시 이동하는 경로
+text='수정' // 텍스트 값
+path='/commission' // 클릭시 이동하는 경로
 addStyle={{
   padding: '11px',  // 값에 ''를 붙여줘야함
   borderRadius: '10px',
