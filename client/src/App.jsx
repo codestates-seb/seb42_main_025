eeab5ea707f69389be22cdaaaa8ddc805d7d772d
benidene <dlgusehd09@naver.com>
2023-03-15 12:04:58 +0900

    ✨ : 커미션 등록페이지 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import theme from './style/theme';
+import Header from 'container/Header';
 import Home from './page/home/Home';
 import Footer from 'container/Footer';
 import Login from 'page/Login';
 import Signup from 'page/Signup';
 import Post from 'page/post/Post';
+import CreatePost from 'page/CreatePost/CreatePost';
 import ComponentBoxesExamples from 'page/ComponentBoxesExamples';
 
 function App() {
@@ -16,12 +18,14 @@ function App() {
       <React.Fragment>
         <Reset />
         <ThemeProvider theme={theme}>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
             <Route path="/component-boxes-examples" element={<ComponentBoxesExamples />} />
             <Route path="/commission" element={<Post />} />
+            <Route path="/createcommission" element={<CreatePost />} />
           </Routes>
           <Footer />
         </ThemeProvider>
