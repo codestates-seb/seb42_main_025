676900607461395c3b7df49766ceb3e19f9a0b52
benidene <dlgusehd09@naver.com>
2023-03-15 16:43:32 +0900

    ✨ : 질문 등록 페이지 개발

---
+++ client/src/page/CreatePost/CreatePost.jsx
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+// import InputComponent from 'component/InputComponent';
+import Button from 'component/Buttons/Button';
+import DragDrop from './Dropzone';
+
+function CreatePost() {
+  return (
+    <Container>
+      <ContentBox>
+        <Content>
+          <ImgBox>
+            <DragDrop />
+          </ImgBox>
+          <PostDetail>
+            제목
+            {/* <InputComponent /> */}
+            <div>소개글 </div>
+            tag
+            {/* <InputComponent /> */}
+          </PostDetail>
+        </Content>
+        <Toast>toast ui</Toast>
+        <ButtonBox>
+          <Button
+            text="수정"
+            path="/commission"
+            addStyle={{
+              padding: '0px',
+              borderRadius: '10px',
+              fontSize: '16px',
+              width: '70px',
+            }}
+          />
+        </ButtonBox>
+      </ContentBox>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: grid;
+  justify-content: center;
+`;
+
+const ContentBox = styled.div`
+  padding-top: 5rem;
+  min-width: 1280px;
+  display: grid;
+  grid-template-columns: repeat(
+    12,
+    minmax(auto, 92px)
+  ); //repeat(6, 1fr)은 1fr 1fr 1fr 1fr 1fr 1fr과 같다.
+  grid-template-rows: repeat(3, minmax(50px, auto));
+  gap: 1rem;
+`;
+
+const Content = styled.div`
+  display: grid;
+  /* justify-content: center; */
+  grid-column: 1 / span 12;
+  grid-row: 1 / span 1;
+  gap: 1rem;
+`;
+
+const ImgBox = styled.div`
+  display: grid;
+  grid-column: 1 / span 8;
+  grid-row: 1 / span 1;
+  /* grid-template-columns: minmax(92px); */
+  height: 700px;
+  /* background-color: coral; */
+`;
+
+const PostDetail = styled.div`
+  display: grid;
+  grid-column: 9 / span 4;
+  grid-row: 1 / span 1;
+`;
+
+const ButtonBox = styled.div`
+  display: grid;
+  grid-column: 11 / span 1;
+  grid-row: 3 / span 1;
+`;
+
+const Toast = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-column: 2 / span 10;
+  grid-row: 2 / span 1;
+`;
+
+export default CreatePost;
