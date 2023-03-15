83b1d77b9239579c1bbd260fae0d504f59568a19
SeungbaeLee <aha224610@gmail.com>
2023-03-15 15:53:21 +0900

    :sparkles: Tag 매핑 추가

--- server/I6E1/src/main/java/main_project_025/I6E1/commission/mapper/CommissionMapper.java
+++ server/I6E1/src/main/java/main_project_025/I6E1/commission/mapper/CommissionMapper.java
@@ -2,18 +2,40 @@ package main_project_025.I6E1.commission.mapper;
 
 import main_project_025.I6E1.commission.dto.CommissionDto;
 import main_project_025.I6E1.commission.entity.Commission;
+import main_project_025.I6E1.tag.entity.CommissionTag;
+import main_project_025.I6E1.tag.entity.Tag;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface CommissionMapper {
+
+    @Named("mapTags")
+    default List<CommissionTag> mapTags(List<String> tags) {
+        List<CommissionTag> commissionTags = new ArrayList<>();
+        for (String tagName : tags) {
+            commissionTags.add(new CommissionTag(new Tag(tagName)));
+        }
+        return commissionTags;
+    }
+    @Named("tagToString")
+    default List<String> tagToString(List<CommissionTag> tags) {
+        return tags.stream().map(CommissionTag::getTagName).collect(Collectors.toList());
+    }
+
+
+    @Mapping(target = "tags", qualifiedByName = "mapTags", source = "tags")//tag test
     Commission commissionPostDtoToCommission(CommissionDto.Post post);
     Commission commissionPatchDtoToCommission(CommissionDto.Patch patch);
 
     @Mapping(source = "member.name", target = "memberName")
     @Mapping(source = "member.email", target = "memberEmail")
+    @Mapping(source = "tags", target = "tags", qualifiedByName = "tagToString")
     CommissionDto.Response commissionToResponse(Commission commission);
 
     List<CommissionDto.Response> commissionToResponses(List<Commission> commissions);
