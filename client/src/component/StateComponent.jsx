4ff9797c73ab77baca8f3830b4ab096d959cc8e3
Diiiiiikey <chest6654@gmail.com>
2023-03-16 16:36:25 +0900

    :recycle: 버튼, 컴포넌트박스, 상태컴포넌트, 컨테이너, 풋터 등 수정

--- client/src/component/StateComponent.jsx
+++ client/src/component/StateComponent.jsx
@@ -1,16 +1,33 @@
 import styled, { css } from 'styled-components';
 
-function StateComponent({ state }) {
-  return <StateSign state={state} />;
+function StateComponent({ state, column, row }) {
+  return (
+    <StyledContainer column={column} row={row}>
+      <StateSign state={state} />
+    </StyledContainer>
+  );
 }
 
+const StyledContainer = styled.div`
+  display: grid;
+  height: 100%;
+  justify-items: center;
+  align-items: center;
+  ${({ column, row }) => {
+    return css`
+      grid-column: ${column};
+      grid-row: ${row};
+    `;
+  }}
+`;
+
 const StateSign = styled.div`
+  display: grid;
   width: 0.5rem;
   height: 0.5rem;
   border-radius: 50%;
   background-color: orange;
-  ${({ theme, state }) => {
-    console.log(theme, state);
+  ${({ state }) => {
     return css`
       background-color: ${state};
     `;
--- client/src/container/Footer.jsx
+++ client/src/container/Footer.jsx
@@ -2,26 +2,25 @@ import styled from 'styled-components';
 
 function Footer() {
   return (
-    <div>
-      <Wrapper>
-        <Main>
-          <Contents>
-            상호: (주)나는 이렇게 살아왔는데 | 대표 신은진 <br />
-            copyright 2023. 나는 이렇게 살아왔는데 All rights reserved.
-          </Contents>
-        </Main>
-      </Wrapper>
-    </div>
+    <Wrapper>
+      <Main>
+        <Contents>
+          상호: (주)나는 이렇게 살아왔는데 | 대표 신은진 <br />
+          copyright 2023. 나는 이렇게 살아왔는데 All rights reserved.
+        </Contents>
+      </Main>
+    </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
+  display: grid;
   width: 100%;
   min-height: 100%;
 `;
 
 const Main = styled.footer`
-  display: flex;
+  display: grid;
   justify-content: center;
   align-items: center;
   height: 10rem;
