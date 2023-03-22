f6d5cc7fc9a3c7110161b860f4a9fc28000c8823
benidene <dlgusehd09@naver.com>
2023-03-22 12:51:34 +0900

    ✨ : 드랍존 중간점검

--- client/src/page/CreatePost/Dropzone.jsx
+++ client/src/page/CreatePost/Dropzone.jsx
@@ -1,12 +1,21 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import styled from 'styled-components';
-import { RxFilePlus } from 'react-icons/rx';
+import { RxFilePlus, RxCross2, RxCheckCircled } from 'react-icons/rx';
 
-function Dropzone() {
+export default function Dropzone() {
   const [files, setFiles] = useState([]);
 
   const onDrop = useCallback(acceptedFiles => {
+    // files, acceptedFiles 비교후 같은게 있는지보고 같은부분이 있다면 그 부분 이용해서 죽이기
+    // for문 고차함수(map, filter) 등으로 바꿔서 작성하기
+    let i = 0;
+    for (i = 0; i < acceptedFiles.name; i++) {
+      if (acceptedFiles.name === files.name) {
+        removeFile(acceptedFiles.name);
+      }
+    }
+    console.log(acceptedFiles);
     if (acceptedFiles?.length) {
       setFiles(previousFiles => [
         ...previousFiles,
@@ -15,76 +24,112 @@ function Dropzone() {
     }
   }, []);
 
-  const images = files.map(file => <Photo key={file.name} src={file.preview} alt="images" />);
+  useEffect(() => {
+    // Revoke the data uris to avoid memory leaks
+    return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
-  const { getRootProps, getInputProps } = useDropzone({
+  const removeFile = name => {
+    setFiles(previousFiles => previousFiles.filter(file => file.name !== name));
+  };
+
+  const images = files.map((file, idx) => (
+    <PhotoRemove key={file.name + idx}>
+      <Photo src={file.preview} alt={file.name} />
+      <Remove onClick={() => removeFile(file.name)}>
+        <RxCross2 size="21" />
+      </Remove>
+    </PhotoRemove>
+  ));
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       'image/*': [],
     },
+    maxFiles: 6,
+    timeout: 300000,
+    addRemoveLinks: true,
+    dictRemoveFile: '삭제',
+    noDragEventsBubbling: true,
     onDrop,
   });
   console.log(images);
+
   return (
     <Container>
-      <Content {...getRootProps()}>
+      <div {...getRootProps()}>
         <input {...getInputProps()} />
-        {images.length < 1 ? (
-          <IconBox>
-            <Icon>
+
+        <IconBox>
+          {isDragActive ? (
+            <RxCheckCircled size="100" color="green" />
+          ) : (
+            <div>
               <RxFilePlus size="100" />
-            </Icon>
-          </IconBox>
-        ) : (
-          <ImgBox>
-            <PhotoBox>{images}</PhotoBox>
-          </ImgBox>
-        )}
-        {/* <ImgBox>
-          <PhotoBox>{images}</PhotoBox>
-        </ImgBox> */}
-      </Content>
+              <Drop>Drop Here</Drop>
+            </div>
+          )}
+        </IconBox>
+      </div>
+      <ImgBox>
+        <PhotoBox>{images}</PhotoBox>
+      </ImgBox>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.form`
   display: grid;
-`;
-
-const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
 `;
 
 const IconBox = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
+  height: 200px;
+  border: 1px dotted black;
 `;
 
-const Icon = styled.div`
-  display: flex;
+const Drop = styled.div`
+  margin-left: 13px;
+  font-weight: 800;
 `;
 
 const ImgBox = styled.div`
   display: flex;
+  height: 470px;
+  border: 1px solid black;
 `;
 
 const PhotoBox = styled.div`
   display: flex;
   flex-wrap: wrap;
+  height: 230px;
+`;
+
+const PhotoRemove = styled.div`
+  display: flex;
+`;
+
+const Remove = styled.button`
+  width: 25px;
+  height: 25px;
+  margin: 1px 0px 0px -20px;
+  padding: 2px 0px 0px 0.5px;
+  border-radius: 50px;
+  border: none;
+  background-color: #ddba9d;
+  color: white;
 `;
 
 const Photo = styled.img`
-  min-width: 278px;
-  max-width: 278px;
+  min-width: 273px;
+  max-width: 273px;
   min-height: 217px;
   max-height: 217px;
-  padding: 1rem;
-  margin: 4rem 0px 4rem 4px;
-  object-fit: cover;
-  border: 1px solid lightgray;
+  margin: 11px 1px 0px 3px;
+  object-fit: contain;
+  border: 1px solid black;
 `;
-
-export default Dropzone;
