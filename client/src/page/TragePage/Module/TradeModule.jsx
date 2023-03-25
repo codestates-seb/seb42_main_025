90618ebd83f145639539cdee3d7aed879f5dbb09
Diiiiiikey <chest6654@gmail.com>
2023-03-26 04:04:29 +0900

    :truck: commissionRequest -> trade 로 이름 변경

---
+++ client/src/page/TragePage/Module/TradeModule.jsx
@@ -0,0 +1,53 @@
+import Typography from 'component/Typography';
+import styled from 'styled-components';
+
+function TradeModule({ info }) {
+  return (
+    <StyledSummaryBox>
+      <StyledImg src={info.image} alt={info.title} />
+      <StyledContentContainer>
+        <Typography
+          text={info.title}
+          variant="h2"
+          bold="bold"
+          size="xxl"
+          line={1}
+          height="h_s"
+          width="w_xxxxl"
+        />
+        <Typography
+          text={info.content}
+          line={4}
+          variant="p"
+          height="h_xl"
+          width="w_xxxxl"
+          lineHeight="base"
+        />
+      </StyledContentContainer>
+    </StyledSummaryBox>
+  );
+}
+
+const StyledSummaryBox = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 1rem;
+  border-bottom: 1rem solid #f0d8ba;
+  white-space: nowrap;
+`;
+
+const StyledImg = styled.img`
+  max-width: 10rem;
+  height: auto;
+  aspect-ratio: 4 / 3;
+  align-self: center;
+`;
+
+const StyledContentContainer = styled.div`
+  display: flex;
+  align-items: start;
+  flex-direction: column;
+  margin-left: 1.5rem;
+`;
+
+export default TradeModule;
