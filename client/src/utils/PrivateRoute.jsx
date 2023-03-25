b42473250390a010e569c92e695b80764b30f4a5
Diiiiiikey <chest6654@gmail.com>
2023-03-25 18:56:21 +0900

    :recycle: signUp 오타 수정

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { useLocation, Navigate } from 'react-router-dom';
 export default function PrivateRoute(props) {
   const location = useLocation();
 
-  if (location.pathname === '/signup' || location.pathname === '/login') {
+  if (location.pathname === '/signUp' || location.pathname === '/login') {
     return props.isLogin ? <Navigate to={'/'} /> : props.component;
   } else {
     return props.isLogin ? props.component : <Navigate to={'/login'} replace />;
