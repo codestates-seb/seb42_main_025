c085cb4b2e8710de59db576b4eacf47f9341d758
Diiiiiikey <chest6654@gmail.com>
2023-03-27 03:43:51 +0900

    :twisted_rightwards_arrows: mergeConflict

--- client/src/Components/Carousel.jsx
+++ client/src/Components/Carousel.jsx
@@ -2,12 +2,8 @@ import styled from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import { getCommissions } from 'apis/api/commissions';
-// import { useEffect, useState } from 'react';
 
 function Carousel({ items, changeCarouselImage }) {
-  // const [commissions, setCommissions] = useState(null);
-
   const settings = {
     dots: true, // 캐러셀이미지가 몇번째인지 알려주는 점을 보여줄지 정한다.
     infinite: true, // loop를 만들지(마지막 이미지-처음 이미지-중간 이미지들-마지막 이미지)
@@ -23,14 +19,6 @@ function Carousel({ items, changeCarouselImage }) {
     // autoplay: true,
     // autoplaySpeed: 1000, //자동으로 슬라이드되게 만들어준다.
   };
-
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     const data = await getCommissions();
-  //     setCommissions(data);
-  //   };
-  //   fetch();
-  // }, [setCommissions]);
 
   const commissions = [{ id: 'dd', url: 'dd' }];
 
