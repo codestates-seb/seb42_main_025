1dd813aac619e7b9539ac29b10f391765de8082d
benidene <dlgusehd09@naver.com>
2023-03-28 12:52:36 +0900

    :recycle: patch, delete 추가

--- client/src/apis/api/commission.js
+++ client/src/apis/api/commission.js
@@ -24,3 +24,33 @@ export const postCommission = async data => {
     console.log(err);
   }
 };
+
+export const patchCommission = async (data, id) => {
+  const token = localStorage.getItem('authorization');
+  try {
+    const res = await imgInstance.patch(`/commission/${id}`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteCommission = async id => {
+  const token = localStorage.getItem('authorization');
+  try {
+    const res = await instance.delete(`/commission/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
