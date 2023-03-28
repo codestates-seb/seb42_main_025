b8bd7deebe8dadf854023f98c3671587c038363d
Diiiiiikey <chest6654@gmail.com>
2023-03-29 06:34:05 +0900

    :recycle: git commistion res.data => res

--- client/src/apis/api/commission.js
+++ client/src/apis/api/commission.js
@@ -4,7 +4,7 @@ export const getCommission = async id => {
   try {
     const res = await instance.get(`/commission/${id}`);
     console.log(res);
-    return res.data;
+    return res;
   } catch (err) {
     console.log(err);
   }
