baf7ef7e223bfb63dd635511f7d601869727d8c0
Diiiiiikey <chest6654@gmail.com>
2023-03-29 10:00:38 +0900

    :sparkles: tags commissions

--- client/src/customHook/getCommissionFetch.js
+++ client/src/customHook/getCommissionFetch.js
@@ -11,7 +11,7 @@ export const getCommissionsFn = () => {
       setCommissions(data);
     };
     fetch();
-  }, [setCommissions]);
+  }, [setCommissions, getCommissions]);
 
   return commissions;
 };
